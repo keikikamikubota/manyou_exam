c71e8d5db3ac67ceb7929c48770e874202b1a6f3
keiki <kkeki0410@gmail.com>
2023-08-31 11:32:33 +0900

    new, create, indexアクションとそのビューファイルを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,42 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクが登録できました"
+    else
+      render :new, notice: "登録に失敗しました"
+    end
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def task_params
+      params.require(:task).permit(:name, :content)
   end
 end
