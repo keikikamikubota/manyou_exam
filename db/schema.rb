e298ba2df7210c46578e19488ba1bec69238f5f9
keiki <kkeki0410@gmail.com>
2023-09-03 15:32:05 +0900

    expired_atカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_31_015118) do
+ActiveRecord::Schema.define(version: 2023_09_03_053128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_08_31_015118) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "expired_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
   end
 
 end
