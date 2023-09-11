d2c558e60f0c1c7b9bbbaa769d72b9942dd6186c
keiki <kkeki0410@gmail.com>
2023-09-11 11:35:59 +0900

    ログイン機能とログアウト機能を追加した

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to pictures_path, notice: "本人以外はユーザー編集ができません"
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "登録が完了しました。"
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path, notice: "ユーザー情報を更新しました"
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
   before_validation { email.downcase! }
+
   has_secure_password
+  validates :password, length: { minimum: 4 }
 end
