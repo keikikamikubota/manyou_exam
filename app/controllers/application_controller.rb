573be45d858bd29d830ceabe153c4696778166c2
keiki <kkeki0410@gmail.com>
2023-09-11 19:33:39 +0900

    管理者ユーザーのindex画面を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
   def login_required
     redirect_to new_session_path unless current_user
   end
+
+  def admin_user?
+    if current_user.admin == false
+      flash[:alert] = "管理者ユーザーでログインしてください"
+      redirect_to new_session_path
+    end
+  end
 end
