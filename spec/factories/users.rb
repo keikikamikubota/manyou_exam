a97be752794a9b1fb467e4f49a77d5353edde21d
keiki <kkeki0410@gmail.com>
2023-09-12 23:35:31 +0900

    Rspecのログイン機能、セッション機能のテストコードを記入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,20 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
+    name { '管理者' }
+    email { "admin1@example.com" }
+    admin { true }
+    password { 'admin1' }
+  end
+
+  factory :second_user, class: User do
+    name { '一般ユーザー2' }
+    email { "ippan2@example.com" }
+    password { 'ippan2'}
+  end
+
+  factory :third_user, class: User do
+    name { '一般ユーザー3' }
+    email { "ippan3@example.com" }
+    password { 'ippan3'}
   end
 end
---
+++ spec/system/user_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :system do
+before do
+  user = FactoryBot.create(:user)
+  user = FactoryBot.create(:second_user)
+  user = FactoryBot.create(:third_user)
+end
+
+  # describe 'ユーザー登録のテスト' do
+  #   context 'ユーザーを新規登録した場合' do
+  #     it 'タスク一覧画面にログインユーザーの名前が見れる' do
+  #       visit new_user_path
+  #       fill_in 'user_name', with: '一般ユーザー4' #fill_inの中は検証ツールのidを参照
+  #       fill_in 'user_email', with: 'ippan4@example.com'
+  #       fill_in 'user_password', with: 'ippan4'
+  #       fill_in 'user_password_confirmation', with: 'ippan4'
+  #       click_on '新規登録'
+  #       sleep 5
+  #       expect(page).to have_content '一般ユーザー4'
+  #     end
+  #   end
+  #   context 'ログインしていない場合' do
+  #     it 'タスク一覧に飛ぼうとしてもログイン画面に遷移される' do
+  #     visit tasks_path
+  #     expect(page).to have_content 'Log in'
+  #     end
+  #   end
+  # end
+
+  describe 'セッション機能のテスト' do
+    let(:user_a){ FactoryBot.create(:user, name: '別ユーザー',
+                  email: 'other@example.com', password: 'other') }
+    before do
+      visit root_path
+      fill_in 'session_email', with: 'ippan2@example.com'
+      fill_in 'session_password', with: 'ippan2'
+      click_on 'Log in'
+    end
+    context 'ログインした際' do
+    #   it "ログインが成功している" do
+    #     visit root_path
+    #     expect(page).to have_content 'タスク一覧'
+    #   end
+    #   it "自分の詳細画面（マイページ）に飛べる" do
+    #     click_on '一般ユーザー2'
+    #     expect(page).to have_content '一般ユーザー2のページ'
+    #   end
+      it '一般ユーザが他人の詳細画面に飛ぶと、タスク一覧画面に遷移する' do
+        visit user_path(user_a.id) #letを使ってidを引き渡し、そこにリンクさせる
+        expect(current_path).to eq tasks_path
+      end
+      it 'ログアウトができる' do
+        click_on 'ログアウト'
+        expect(current_path).to eq new_session_path
+      end
+    end
+  end
+end
